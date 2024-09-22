ae3526df0de9695a2586eac3f0fd0a56bd714c46
Steriousnull <steriousnull@gmail.com>
2024-09-22 18:58:52 +0530

    Kick start

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,20 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
+  standalone: true,  
+  imports: [RouterOutlet, FormsModule],
   templateUrl: './app.component.html',
+  //template: "<h1>Hey all..!!</h1>",
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'AngularProject';
+  //title = 'homes';
+  fullName = "testing input" 
+
+  getName(){
+    return "me";
+  }
 }
